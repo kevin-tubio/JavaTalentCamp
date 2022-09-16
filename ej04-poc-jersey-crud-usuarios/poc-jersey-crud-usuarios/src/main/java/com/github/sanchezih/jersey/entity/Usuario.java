cd148a39ed2ce24a2ea578dd7c35583cfe0f8e7c
Kevin Eloy Tubio <kevtubio@emeal.nttdata.com>
2022-09-16 10:04:02 -0300

    solucion ejercicio 4

---
+++ ej04-poc-jersey-crud-usuarios/poc-jersey-crud-usuarios/src/main/java/com/github/sanchezih/jersey/entity/Usuario.java
@@ -0,0 +1,36 @@
+package com.github.sanchezih.jersey.entity;
+
+import java.io.Serializable;
+
+public class Usuario implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String username;
+	private String name;
+
+	public Usuario() {
+	}
+
+	public Usuario(String username, String name) {
+		super();
+		this.username = username;
+		this.name = name;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ ej04-poc-jersey-crud-usuarios/poc-jersey-crud-usuarios/src/main/java/com/github/sanchezih/jersey/service/UsuarioService.java
@@ -0,0 +1,81 @@
+package com.github.sanchezih.jersey.service;
+
+import java.util.*;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import com.github.sanchezih.jersey.entity.Usuario;
+
+@Path("/users")
+public class UsuarioService {
+
+	private static final Map<String, Usuario> USUARIOS = new HashMap<>();
+
+	public UsuarioService() {
+		USUARIOS.put("mperez", new Usuario("mperez", "Mariela"));
+		USUARIOS.put("jlopez", new Usuario("jlopez", "Juan"));
+		USUARIOS.put("savila", new Usuario("savila", "Sol"));
+	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getUsers() {
+		return Response.ok(new ArrayList<>(USUARIOS.values())).build();
+	}
+
+	@GET
+	@Path("/{username}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getUserByUsername(@PathParam("username") String username) {
+		if (USUARIOS.get(username) == null) {
+			return Response.status(Status.BAD_REQUEST).entity("Usuario no encontrado").build();
+		}
+
+		return Response.ok(USUARIOS.get(username)).build();
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response createUser(Usuario userRequest) {
+		USUARIOS.put(userRequest.getUsername(), userRequest);
+		return Response.ok(new ArrayList<>(USUARIOS.values())).build();
+	}
+
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response updateUser(Usuario usuario) {
+		Usuario found = USUARIOS.get(usuario.getUsername());
+		if (found == null) {
+			return Response.status(Status.BAD_REQUEST).entity("Usuario no encontrado").build();
+		}
+
+		found.setName(usuario.getName());
+		return Response.ok(found).build();
+	}
+
+	@DELETE
+	@Path("/{username}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response deleteUser(@PathParam("username") String username) {
+		Usuario found = USUARIOS.get(username);
+		if (found == null) {
+			return Response.status(Status.BAD_REQUEST).entity("Usuario no encontrado").build();
+		}
+
+		USUARIOS.remove(username);
+		return Response.ok(new ArrayList<>(USUARIOS.values())).build();
+	}
+
+}
